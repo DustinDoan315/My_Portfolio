0bf116f5c69e48506c992e8bf7c641686bf758ac
Dustin Doan <doankhanhdong3105@gmail.com>
2024-10-29 15:30:29 +0700

    update minor codes

--- my-portfolio/src/app/blogs/page.tsx
+++ my-portfolio/src/app/blogs/page.tsx
@@ -1,12 +1,11 @@
 import Layout from "../page";
 
-const ProjectDetailPage = () => {
+const Blogs = () => {
   return (
     <Layout>
       <h1>Project Detail</h1>
-      <p>Showing details for project ID</p>
     </Layout>
   );
 };
 
-export default ProjectDetailPage;
+export default Blogs;
