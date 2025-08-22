0865d32b827f02e63d8f3fac543028d34052b8a1
Dustin Doan <doankhanhdong3105@gmail.com>
2025-08-22 10:25:21 +0700

    update images mode

--- my-portfolio/src/app/projects/page.tsx
+++ my-portfolio/src/app/projects/page.tsx
@@ -76,7 +76,7 @@ const ProjectPage = () => {
                     <img
                       src={item.image}
                       alt={item.title}
-                      className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
+                      className="w-full h-48 object-contain group-hover:scale-105 transition-transform duration-300"
                     />
                     <div className="absolute top-4 right-4 flex items-center space-x-2">
                       <span
